7b78511947deeaca3ce7b86a8994887424c93fb7
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-12 12:13:35 +0530

    [Ruchi]: Added Lose or win Feature implemented

--- src/com/bridgelabz/workshop/Gambler.java
+++ src/com/bridgelabz/workshop/Gambler.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.workshop;
 
 public class Gambler {
-    public static void main(String[] args) {
-        public static final int $Daily = 100;
-        public static final int $Pergame = 1;
+    public static final int $DAILY = 100;
+    public static final int $PERGAME = 1;
+    public static final int WIN = 1;
+    public static final int LOSE = 0;
+    public static void main(String[] args)
+    {
+        double Action = Math.floor(Math.random() * 10) % 2;
+        if(Action==WIN)
+        {
+            System.out.println("You won $1");
+
+        }
+        else if(Action==LOSE)
+        {
+            System.out.println("You lose $1");
+        }
 
     }
 }
