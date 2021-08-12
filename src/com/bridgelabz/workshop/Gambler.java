8d33e6e3ad1612f46daa6c347de32ce917b21b51
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-12 12:23:34 +0530

    [Ruchi]: Inititalised variables

--- src/com/bridgelabz/workshop/Gambler.java
+++ src/com/bridgelabz/workshop/Gambler.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.workshop;
 
 public class Gambler {
+    public static final int $DAILY = 100;
+    public static final int $PERGAME = 1;
     public static void main(String[] args) {
-        public static final int $DAILY = 100;
-        public static final int $PERGAME = 1;
+
 
     }
 }
