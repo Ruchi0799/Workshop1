4616b7bed988ff8a38ede456d42161806b01594c
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-12 11:34:17 +0530

    UC1 done

---
+++ src/com/bridgelabz/workshop/Gambler.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.workshop;
+
+public class Gambler {
+    public static void main(String[] args) {
+        public static final int $Daily = 100;
+        public static final int $Pergame = 1;
+
+    }
+}
